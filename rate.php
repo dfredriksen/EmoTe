ed42a3a70d71b6e5eec1f72538e93d0666dd3611
Daniel Fredriksen <dfredriksen@dnainfo.com>
2015-10-16 05:21:45 -0400

    Added a program that scores a winning emotion

---
+++ rate.php
@@ -0,0 +1,49 @@
+<?php
+
+    ini_set('include_path', '/home/dfredriksen/EmoTe' . ini_get('include_path'));
+    require_once('/home/dfredriksen/EmoTe/Eklekt/Emotion.php');
+    require_once('/home/dfredriksen/EmoTe/Eklekt/Emotion/Empathyscope.php');
+    require_once('/home/dfredriksen/EmoTe/Eklekt/Emotion/EmotionalState.php');
+    require_once('/home/dfredriksen/EmoTe/Eklekt/Emotion/AffectWord.php');
+
+    require_once('/home/dfredriksen/EmoTe/Eklekt/Emotion/Utility/Heuristics.php');
+    require_once('/home/dfredriksen/EmoTe/Eklekt/Emotion/Utility/Lexical.php');
+
+
+    $configs = parse_ini_file('config.ini');
+    
+    $host = $configs['database'];
+    $user = $configs['user'];
+    $pass = $configs['password'];
+    $db = new PDO("mysql:host=$host;dbname=Analysis;charset=utf8", $user, $pass);
+    $result = $db->query('SELECT row_id, neutral, happy, sad, fear, anger, disgust, surprise from Top10');
+    while( $row = $result->fetch(PDO::FETCH_ASSOC))
+    {
+        $id = $row['row_id'];
+
+        $emotions = array(
+            '0' => $row['neutral'],
+            '1' => $row['happy'],
+            '2' => $row['sad'],
+            '3' => $row['fear'],
+            '4' => $row['anger'],
+            '5' => $row['disgust'],
+            '6' => $row['surprise']
+        );
+
+        $winner = -1;
+        $weight = -1;
+
+        foreach($emotions as $emotion=>$value) 
+        {
+            if($value > $weight) {
+                $winner = $emotion;
+                $weight = $value;
+            }
+        }
+
+        echo "Row $id is a $winner with a weight of $weight.\n";
+        $db->exec("UPDATE Top10 set primary_emotion=$winner where row_id=$id");
+    }
+
+ echo "Done!";
